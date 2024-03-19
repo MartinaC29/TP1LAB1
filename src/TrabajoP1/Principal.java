3fd81e119f037ca8e752d6c24dfe96aa6ca232e9
Martina Callegaro <marticallegaro29j@gmail.com>
2024-03-19 13:40:17 -0300

    Initial commit

---
+++ src/TrabajoP1/Principal.java
@@ -0,0 +1,15 @@
+
+package TrabajoP1;
+
+import java.util.Scanner;
+
+public class Principal {
+    
+    public static void main (String[] args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Practico repositorio");
+        System.out.println("Esto es una prueba, escriba algo");
+        String leer = teclado.nextLine();
+        
+    }
+}
